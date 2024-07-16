548396bd1297a22d92fbb090cc32275e5e6ad190
Christian <chrisdaq@gmail.com>
2024-07-16 17:30:26 -0400

    set model many to many relationship and dump result to test things out

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Employee;
 
 Route::get('/', function () {
+    $employee = Employee::find(2);
+
+    dd($employee->services);
     return view('welcome');
 });
