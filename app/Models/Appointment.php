de99209c42c4831f17f15c820215cdb1e900a813
Christian <chrisdaq@gmail.com>
2024-07-17 17:04:22 -0400

    add appointment model, related relationships and appointment factory

---
+++ app/Models/Appointment.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected function casts():array
+    {
+        return [
+            'starts_at' => 'datetime',
+            'ends_at' => 'datetime',
+        ];
+    }
+
+    protected $guarded = [];
+
+    /** hook into enloquent event
+     * while appointment is being created, automatically generate uuid
+     *
+     */
+    public static function booted()
+    {
+        static::creating(function (Appointment $appointment) {
+            $appointment->uuid = str()->uuid();
+        });
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
