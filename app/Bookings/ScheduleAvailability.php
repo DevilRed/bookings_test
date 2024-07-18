8d446bf0f9b55b06c90cc9dec8ca4bc6e5ac7d60
Christian <chrisdaq@gmail.com>
2024-07-18 18:10:12 -0400

    check availabilities for an employee and a service

--- app/Bookings/ScheduleAvailability.php
+++ app/Bookings/ScheduleAvailability.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Bookings;
 
+use App\Models\Employee;
+use App\Models\Service;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use Spatie\Period\Boundaries;
@@ -11,7 +13,7 @@ use Spatie\Period\Precision;
 class ScheduleAvailability
 {
     protected PeriodCollection $periods;
-    public function __construct()
+    public function __construct(protected Employee $employee, protected Service $service)
     {
         // custom period collection
         $this->periods = new PeriodCollection();
@@ -19,10 +21,41 @@ class ScheduleAvailability
 
     public function forPeriod(Carbon $startsAt, Carbon $endsAt)
     {
-        // iterate over the days of a period to check if an employee is available on a day
+        // use collect to iterate over the days of a period to check if an employee is available on that day
         collect(CarbonPeriod::create($startsAt, $endsAt)->days())
             ->each(function ($date) {
-                dump($date->format('l'));
+                $this->addAvailabilityFromSchedule($date);
             });
+        dd($this->periods);
+    }
+
+    /**
+     * Check if an employee can work on given days
+     * @param Carbon $date
+     * @return void
+     */
+    protected function addAvailabilityFromSchedule(Carbon $date): void
+    {
+        // if there is no schedule for the $date just return
+        if (!$schedule = $this->employee->schedules()
+            ->where('starts_at', '<=', $date)
+            ->where('ends_at', '>=', $date)->first()) {
+            return;
+        }
+        // schedule is a collection,
+        // check for empty results
+        if(![$startsAt, $endsAt] = $schedule->getWorkingHoursForDate($date)) {
+            return;
+        }
+        // add a valid period
+        $this->periods = $this->periods->add(
+            Period::make(
+                // use copy() to not mutate the value of the current date
+                $date->copy()->setTimeFromTimeString($startsAt),
+                // take into account the service duration time to avoid inconsistencies and give wrong time periods
+                $date->copy()->setTimeFromTimeString($endsAt)->subMinutes($this->service->duration),
+                Precision::MINUTE()
+            )
+        );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Employee;
+use App\Models\Service;
 
 Route::get('/', function () {
-    $availability = (new \App\Bookings\ScheduleAvailability())
+    $employee = Employee::find(1);
+    $service = Service::find(1);
+    $availability = (new \App\Bookings\ScheduleAvailability($employee, $service))
         ->forPeriod(
             now()->startOfDay(),
             now()->addMonth()->endOfDay()
