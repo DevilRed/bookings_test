4fb1e9d3aad37b9b9d31618d028a22c26b2ec8d9
Christian <chrisdaq@gmail.com>
2024-07-23 14:50:56 -0400

    test different daily schedule times

--- tests/Feature/Bookings/ScheduleAvailabilityTest.php
+++ tests/Feature/Bookings/ScheduleAvailabilityTest.php
@@ -28,6 +28,7 @@ it('list correct employee availability', function () {
     $availability = (new ScheduleAvailability($employee, $service))
         ->forPeriod(now()->startOfDay(), now()->endOfDay());
 
+    // ->current() get the first item in the collection
     expect($availability->current())
         // startsAt is a method on the period
         ->startsAt(now()->setTimeFromTimeString('09:00:00'))
@@ -36,4 +37,42 @@ it('list correct employee availability', function () {
         ->toBeTrue();
 });
 
-// from 12. min 7.45
+it('accounts for different daily schedule times', function () {
+    // For testing purposes set the date to a specific date
+    Carbon::setTestNow(Carbon::parse('Monday January 2000'));
+    // create employee
+    $employee = Employee::factory()
+        ->has(Schedule::factory()->state([
+            'starts_at' => now()->startOfDay(),
+            'ends_at' => now()->addYear()->endOfDay(),
+            'monday_starts_at' => '11:00:00',
+            'monday_ends_at' => '16:00:00',
+            'tuesday_starts_at' => '09:00:00',
+            'tuesday_ends_at' => '17:00:00',
+        ]))
+        ->create();
+
+    // create service
+    $service= Service::factory()
+        ->create(['duration' => 30]);
+
+    $availability = (new ScheduleAvailability($employee, $service))
+        ->forPeriod(now()->startOfDay(), now()->addDay()->endOfDay());
+
+    expect($availability->current())
+        ->startsAt(now()->setTimeFromTimeString('11:00:00'))
+        ->toBeTrue()
+        ->endsAt(now()->setTimeFromTimeString('15:30:00'))
+        ->toBeTrue();
+
+    // go to the next day
+    $availability->next();
+
+    expect($availability->current())
+        ->startsAt(now()->addDay()->setTimeFromTimeString('09:00:00'))
+        ->toBeTrue()
+        ->endsAt(now()->addDay()->setTimeFromTimeString('16:30:00'))
+        ->toBeTrue();
+});
+
+// from 12. min 8.09
