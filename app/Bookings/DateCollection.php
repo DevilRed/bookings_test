fbd156e726e570ef6c54bdbb9fac8036e37265c0
Christian <chrisdaq@gmail.com>
2024-07-31 18:26:56 -0400

    add custom date collection to implement custom helpers

---
+++ app/Bookings/DateCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Bookings;
+
+use Illuminate\Support\Collection;
+
+class DateCollection extends Collection
+{
+    /**
+     * Iterate over all dates, return the first where slots are available
+     * @return \Closure|null
+     */
+    public function firstAvailableDate()
+    {
+        return $this->first(function (Date $date) {
+            return $date->slots->count() >= 1;
+        });
+    }
+}
