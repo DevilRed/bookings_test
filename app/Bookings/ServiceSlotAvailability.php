b7382255db614571e4c7fcc7916dc235a1c927aa
Christian <chrisdaq@gmail.com>
2024-07-30 21:49:32 -0400

    add employees into slots

---
+++ app/Bookings/ServiceSlotAvailability.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Bookings;
+
+use App\Models\Employee;
+use App\Models\Service;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use Spatie\Period\Period;
+use App\Bookings\SlotRangeGenerator;
+
+class ServiceSlotAvailability
+{
+
+    /**
+     * hold what employees can fulfill the availability
+     * @param Collection $employees
+     * @param Service $service
+     */
+    public function __construct(protected Collection $employees, protected Service $service)
+    {
+    }
+
+    public function forPeriod(Carbon $startsAt, Carbon $endsAt)
+    {
+        $range = (new SlotRangeGenerator($startsAt, $endsAt))->generate($this->service->duration);
+
+        $this->employees->each(function (Employee $employee) use ($startsAt, $endsAt, &$range) {
+            // get the availability for the employee
+            $periods = (new ScheduleAvailability($employee, $this->service))
+                ->forPeriod($startsAt, $endsAt);
+            foreach ($periods as $period) {
+                $this->addAvailableEmployeeForPeriod($range, $period, $employee);
+            }
+            // remove appointments from the period collection
+            // add the available employees to the range
+            // remove empty slots
+        });
+
+        return $range;
+    }
+
+
+    protected function addAvailableEmployeeForPeriod(Collection $range, Period $period, Employee $employee)
+    {
+        $range->each(function (Date $date) use($period, $employee) {
+            // iterate over each slot of date and see if an employee can fulfill that slot based on period
+            $date->slots->each(function (Slot $slot) use ($period, $employee) {
+                // period contains the slot time
+                if ($period->contains(($slot->time))) {
+                    $slot->addEmployee($employee);
+                }
+            });
+        });
+    }
+}
--- app/Bookings/Slot.php
+++ app/Bookings/Slot.php
@@ -2,6 +2,7 @@
 
 namespace App\Bookings;
 
+use App\Models\Employee;
 use Carbon\Carbon;
 
 /**
@@ -9,8 +10,13 @@ use Carbon\Carbon;
  */
 class Slot
 {
-    public $employees;
+    public $employees = [];
     public function __construct(public Carbon $time)
     {
+    }
+
+    public function addEmployee(Employee $employee)
+    {
+        $this->employees[] = $employee;
     }
 }
