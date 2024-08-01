6c626bca3739f6bf0c8b99bc05a1a232a15b5d3b
Christian <chrisdaq@gmail.com>
2024-08-01 16:43:14 -0400

    add more tests

--- tests/Feature/Bookings/ServiceSlotAvailabilityTest.php
+++ tests/Feature/Bookings/ServiceSlotAvailabilityTest.php
@@ -103,3 +103,65 @@ it('excludes booked appointments for the employee', function () {
         ->toContain('13:00:00');
 });
 
+it('ignores cancelled appointments', function () {
+    Carbon::setTestNow(Carbon::parse('1st January 2000'));
+
+    // create service
+    $service= Service::factory()
+        ->create(['duration' => 30]);
+
+    $employee = Employee::factory()
+        ->has(Schedule::factory()->state([
+            'starts_at' => now()->startOfDay(),
+            'ends_at' => now()->endOfDay(),
+        ]))
+        ->has(Appointment::factory()->for($service)->state([
+            'starts_at' => now()->setTimeFromTimeString('12:00'),
+            'end_at' => now()->setTimeFromTimeString('12:45'),
+            'cancelled_at' => now(),
+        ]))
+        ->create();
+
+
+
+    $availability = (new ServiceSlotAvailability(collect([$employee]), $service))
+        ->forPeriod(now()->startOfDay(), now()->endOfDay());
+
+    // get a list of slots in the array
+    // map through all the dates
+    $slots = $availability->map(function (Date $date) {
+        // for each return the slot to compare it's time
+        return $date->slots->map(fn (Slot $slot) => $slot->time->toTimeString());
+    })
+        ->flatten()
+        ->toArray();
+    expect($slots)
+        ->toContain('11:30:00')
+        // slots should include already cancelled appointments
+        ->toContain('12:00:00')
+        ->toContain('12:30:00')
+        ->toContain('13:00:00');
+});
+
+it('shows multiple employees available for a service', function () {
+    Carbon::setTestNow(Carbon::parse('1st January 2000'));
+
+    // create service
+    $service= Service::factory()
+        ->create(['duration' => 30]);
+
+    $employees = Employee::factory()
+        ->count(2)
+        ->has(Schedule::factory()->state([
+            'starts_at' => now()->startOfDay(),
+            'ends_at' => now()->endOfDay(),
+        ]))
+        ->create();
+
+
+
+    $availability = (new ServiceSlotAvailability($employees, $service))
+        ->forPeriod(now()->startOfDay(), now()->endOfDay());
+
+    expect($availability->first()->slots->first()->employees)->toHaveCount(2);
+});
