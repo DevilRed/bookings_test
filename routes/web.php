e7a6d2b4b8ed68dac31b8e59ce201378d789e8e4
Christian <chrisdaq@gmail.com>
2024-08-04 11:48:50 -0400

    add controller, load it as default route

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Employee;
 use App\Models\Service;
+use App\Http\Controllers\BookingController;
 
-Route::get('/', function () {
-    $employee = Employee::find(1);
-    $service = Service::find(1);
-    $availability = (new \App\Bookings\ScheduleAvailability($employee, $service))
-        ->forPeriod(
-            now()->startOfDay(),
-            now()->addMonth()->endOfDay()
-        );
-});
+Route::get('/', BookingController::class)->name('booking');
