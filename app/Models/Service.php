8a3dc7bc23514fe7a8be9338e9aa7904d9933e06
Christian <chrisdaq@gmail.com>
2024-08-05 17:50:15 -0400

    define custom accessor for service's price attribute to render it with currency simbol

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Service extends Model
 {
@@ -12,5 +13,13 @@ class Service extends Model
     public function employees()
     {
         return $this->belongsToMany(Employee::class);
+    }
+
+    // custom accesors for money formatting
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn (int $price) => '$' . number_format($price / 100, 2, '.', ' '),
+        );
     }
 }
