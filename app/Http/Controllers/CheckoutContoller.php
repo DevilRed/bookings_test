62fa7aef558aebcf93aa915397bf2afc1c97398d
Christian <chrisdaq@gmail.com>
2024-08-13 15:49:31 -0400

    use LockPlugin to disable unavailable dates on calendar, filter availableDates by slots and remap them with date and slot's count as value

--- app/Http/Controllers/CheckoutContoller.php
+++ app/Http/Controllers/CheckoutContoller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Bookings\ServiceSlotAvailability;
 use App\Models\Employee;
 use App\Models\Service;
-use Illuminate\Http\Request;
+use App\Bookings\Date;
 
 class CheckoutContoller extends Controller
 {
@@ -18,7 +18,16 @@ class CheckoutContoller extends Controller
                 now()->addMonth()->endOfDay(),
             );
         $firstAvailableDate = $availability->firstAvailableDate()->date->toDateString();
+        $availableDates = $availability
+            ->hasSlots()
+            ->mapWithKeys(
+                fn(Date $date) => [ $date->date->toDateString() => $date->slots->count()
+                ]
+            )
+            ->toArray()
+        ;
+        ;
         return view('bookings.checkout',
-            compact('employee', 'service', 'firstAvailableDate'));
+            compact('employee', 'service', 'firstAvailableDate', 'availableDates'));
     }
 }
