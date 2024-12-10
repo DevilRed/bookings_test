a44df763d1a22f4f91b5d7a10acdb9a7958baa68
Christian <chrisdaq@gmail.com>
2024-12-10 18:01:06 -0400

    add route to get slots for given date

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,12 @@ use App\Models\Service;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\BookingEmployeeController;
 use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\SlotController;
+
+// fake date for all app to help testing
+\Carbon\Carbon::setTestNow(now()->addDay()->setTimeFromTimeString('09:00:00'));
 
 Route::get('/', BookingController::class)->name('bookings');
 Route::get('/bookings/{employee:slug}', BookingEmployeeController::class)->name('bookings.employee');
 Route::get('/checkout/{employee:slug}/{service:slug}', CheckoutController::class)->name('checkout');
+Route::get('/slots/{employee:slug}/{service:slug}', SlotController::class)->name('slots');
