d9cff3642e603ee895ca8b791573c7429ebb6732
Christian <chrisdaq@gmail.com>
2024-12-11 15:31:11 -0400

    output slot response on an alpine template

--- resources/views/bookings/checkout.blade.php
+++ resources/views/bookings/checkout.blade.php
@@ -103,8 +103,12 @@
             x-on:slots-requested.window="fetchSlots(event)"
         >
             <h2 class="text-xl font-medium mt-3">2. Choose a time slot</h2>
-            <div class="mt-6">
-                slots
+            <div class="mt-6" x-show="slots.length">
+                <div class="grid grid-cols-3 md:grid cols-5 gap-8 mt-6">
+                    <template x-for="slot in slots">
+                        <div x-text="slot" class="py-3  px-4 text-sm border border-slate-200 rounded-lg text-center hover:bg-gray-50/75 cursor-pointer"></div>
+                    </template>
+                </div>
             </div>
         </div>
     </div>
